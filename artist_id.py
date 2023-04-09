adf4212decd1c10b69204d9f53471fac03b5531a
vish <vish0290@gmail.com>
2023-04-09 21:44:38 +0530

    artist id finding code

---
+++ artist_id.py
@@ -0,0 +1,26 @@
+import requests, re
+from bs4 import BeautifulSoup
+
+
+def get_id(artist):    
+    url = ("https://www.google.com/search?q="+artist+"spotify")
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser') 
+    url = []
+    for link in soup.find_all('a'):
+        a = link.get('href')
+        if a.startswith('/url?q=https://open.spotify.com/artist/'):
+            url.append(a)
+            
+    data = url[0]
+    data = data[7:61]
+    md = data.split('/')
+    return md[-1]
+
+
+
+artist = input('Enter the artist name: ')
+x = get_id(artist)
+print(x)
+
